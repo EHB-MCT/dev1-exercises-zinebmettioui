97f28229623faf8fdadec7862f105a1910d812a8
zineb mettioui <zineb.mettioui@student.ehb.be>
2022-10-10 23:19:41 +0200

    1-4 Layers

--- Exercises/1-functions/1-4.js
+++ Exercises/1-functions/1-4.js
@@ -1,1 +1,52 @@
+"use strict";
 
+drawLine();
+
+function drawLine() {
+
+   let canvas = document.querySelector("canvas");
+   canvas.width = window.innerWidth;
+   canvas.height = window.innerHeight;
+   let context = canvas.getContext("2d");
+
+   context.beginPath();
+   context.lineWidth = 3;
+   context.fillStyle = 'lightblue';
+   context.beginPath();
+   context.rect(50,50,350,350);
+   context.fill();
+   context.stroke();
+
+   context.beginPath();
+   context.lineWidth = 3;
+   context.fillStyle = 'lightblue';
+   context.beginPath();
+   context.rect(50,50,300,300);
+   context.fill();
+   context.stroke();
+
+   context.beginPath();
+   context.lineWidth = 3;
+   context.fillStyle = 'lightblue';
+   context.beginPath();
+   context.rect(100,50,250,250);
+   context.fill();
+   context.stroke();
+
+   context.beginPath();
+   context.lineWidth = 3;
+   context.fillStyle = 'lightblue';
+   context.beginPath();
+   context.rect(150,100,200,200);
+   context.fill();
+   context.stroke();
+
+   context.beginPath();
+   context.lineWidth = 3;
+   context.fillStyle = 'lightblue';
+   context.beginPath();
+   context.rect(150,150,150,150);
+   context.fill();
+   context.stroke();
+   
+}
