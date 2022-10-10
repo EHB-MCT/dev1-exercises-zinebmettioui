a00ada4fd1bf0035a9250c7089aaed28942319d8
zineb mettioui <zineb.mettioui@student.ehb.be>
2022-10-10 22:18:35 +0200

    1-2 Created a bow with black and red lines

--- Exercises/1-functions/1-2.js
+++ Exercises/1-functions/1-2.js
@@ -1,1 +1,38 @@
+"use strict";
+
+drawLine();
+
+function drawLine() {
+
+   let canvas = document.querySelector("canvas");
+   canvas.width = window.innerWidth;
+   canvas.height = window.innerHeight;
+   let context = canvas.getContext("2d");
+
+
+   context.beginPath();
+   context.lineWidth = 5;
+   context.strokeStyle = 'red';
+   context.moveTo(50,50)
+   context.lineTo(200,200)
+   context.stroke();
+
+   context.beginPath();
+   context.lineWidth = 5;
+   context.strokeStyle = 'red';
+   context.moveTo(50,200)
+   context.lineTo(200,50)
+   context.stroke();
+   
+   context.beginPath();
+   context.lineWidth = 5;
+   context.strokeStyle = 'Black';
+   context.beginPath();
+   context.rect(50,50,150,150);
+   context.stroke();
+
+   
+
+
+}
 
