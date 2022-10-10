3339a9ccae1a770ff01522d4320d425b5056bdaf
zineb mettioui <zineb.mettioui@student.ehb.be>
2022-10-11 00:05:38 +0200

    1-5b Ellipse

--- Exercises/1-functions/1-5b.js
+++ Exercises/1-functions/1-5b.js
@@ -1,1 +1,32 @@
+"use strict";
 
+drawLine();
+
+function drawLine() {
+
+   let canvas = document.querySelector("canvas");
+   canvas.width = window.innerWidth;
+   canvas.height = window.innerHeight;
+   let context = canvas.getContext("2d");
+
+   
+
+   context.beginPath();
+   context.fillStyle = 'orange';
+   context.beginPath();
+   context.rect(50,50,330,300);
+   context.fill();
+
+   context.beginPath();
+   context.fillStyle = 'blue';
+   context.beginPath();
+   context.rect(50,250,330,100);
+   context.fill();
+
+   context.beginPath();
+   context.fillStyle = 'yellow';
+   context.ellipse(220,250,100,50, Math.PI / 1, 0, Math.PI);
+   context.fill();
+
+
+}
