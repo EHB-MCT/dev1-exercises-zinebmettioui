5e9be7e5288fb7fd5083f4dd16c737b649e5b1be
zineb mettioui <zineb.mettioui@student.ehb.be>
2022-12-07 12:01:08 +0100

    excercises 3

---
+++ Exercises/3-iteration-while/3-1-a.js
@@ -0,0 +1,25 @@
+"use strict";
+import context from "../../scripts/context.js";
+import * as Utils from "../../scripts/utils.js";
+
+draw();
+
+function draw() {
+    context.lineWidth = 2;
+    context.strokeStyle = "white";
+    context.fillStyle = "#3498DB";
+    context.fillRect(50, 50, 300, 300);
+    drawLines();
+}
+
+function drawLines() {
+    let i = ;
+    while () {
+        //This drawLine function does not exist yet!
+        //Add it to the utils script:
+        //The function draws a line between 2 points
+        //And has the following parameters: x1, y1, x2, y2
+        Utils.drawLine(, , , );
+        i = ;
+    }
+}
---
+++ Exercises/3-iteration-while/3-1-e.js
@@ -0,0 +1,22 @@
+"use strict";
+import context from "../../scripts/context.js";
+import * as Utils from "../../scripts/utils.js";
+
+draw();
+
+function draw() {
+    context.lineWidth = 2;
+    context.strokeStyle = "white";
+    context.fillStyle = "#3498DB";
+    context.fillRect(50, 50, 300, 300);
+    drawDiagonalGrid();
+}
+
+function drawDiagonalGrid() {
+    let i = ;
+    while (i <= ) {
+        Utils.drawLine(, , , );
+        Utils.drawLine(, , , );
+        i = ;
+    }
+}
