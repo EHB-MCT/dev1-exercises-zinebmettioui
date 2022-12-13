601f4eef35045ebc11757a5d8814ce839a6633f4
zineb mettioui <zineb.mettioui@student.ehb.be>
2022-12-13 21:15:18 +0100

    3-1-s

--- Exercises/3-iteration-while/3-1-s.js
+++ Exercises/3-iteration-while/3-1-s.js
@@ -12,9 +12,9 @@ function draw() {
 function drawShrinkingBars() {
     let i = 0;
     while (i <= 14) {
-        let gray = ;
+        let gray = 255 - i / 14 * 255;
         context.fillStyle = Utils.rgb(gray, gray, gray);
-        context.fillRect(, , , );
+        context.fillRect(50 + i * 20, 50 + i * 10, 10, 300 - i * 20);
         i++;
     }
 }
