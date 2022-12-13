1c8b24f0cd8909e39940bc11fa69b7427231b0ff
zineb mettioui <zineb.mettioui@student.ehb.be>
2022-12-13 20:25:26 +0100

    3-1-o

--- Exercises/3-iteration-while/3-1-o.js
+++ Exercises/3-iteration-while/3-1-o.js
@@ -14,10 +14,10 @@ function draw() {
 
 function drawGradient() {
     let i = 0;
-    while (i <= ) {
-        let gray = ;
+    while (i <=  300) {
+        let gray = i / 300 * 255;
         context.strokeStyle = Utils.rgb(gray, gray, gray);
-        Utils.drawLine(, , , );
+        Utils.drawLine(50 + i, 50, 50 + i, 350);
         i++;
     }
 }
