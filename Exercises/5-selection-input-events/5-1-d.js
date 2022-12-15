3989b1bbf2d9506b9c23ac2d5a2e42c8f047830c
zineb mettioui <zineb.mettioui@student.ehb.be>
2022-12-15 10:18:26 +0100

    5-1-d

---
+++ Exercises/5-selection-input-events/5-1-d.js
@@ -0,0 +1,20 @@
+"use strict";
+import context from "../../scripts/context.js";
+import * as Utils from "../../scripts/utils.js";
+
+let width = context.canvas.width;
+let height = context.canvas.height;
+
+function drawDotsD() {
+    context.fillRect(0, 0, width, height);
+    for (let i = 0; i < 100000; i++) {
+        let x = Math.random() * width;
+        let y = Math.random() * height;
+        if (x < width / 4 * 3 && y < height / 4 * 3 && x > width / 4 && y > height / 4) {
+            context.fillStyle = "white";
+        } else {
+            context.fillStyle = "blue";
+        }
+        Utils.fillCircle(x, y, 2);
+    }
+}
