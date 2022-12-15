bfb550a97f9c3ae9adcfa58dc656d9635ca1ba55
zineb mettioui <zineb.mettioui@student.ehb.be>
2022-12-15 10:24:26 +0100

    5-2-a-b

---
+++ Exercises/5-selection-input-events/5-2-a.js
@@ -0,0 +1,37 @@
+"use strict";
+import context from "../../scripts/context.js";
+import * as Utils from "../../scripts/utils.js";
+import * as Noise from "../../scripts/noise.js";
+
+let width = context.canvas.width;
+let height = context.canvas.height;
+
+drawMaze();
+
+
+function drawMaze() {
+    context.fillStyle = "darkgray";
+    context.lineCap = "square";
+    context.lineWidth = 5;
+    context.fillRect(0, 0, width, height);
+    let space = 10;
+    for (let i = 0; i < width / space; i++) {
+        for (let j = 0; j < height / space; j++) {
+            let r = Math.floor(Math.random() * 2);
+            if (r == 0) {
+                drawLine(i * space, j * space, (i + 1) * space, (j + 1) * space);
+            } else {
+                drawLine(i * space, (j + 1) * space, (i + 1) * space, j * space);
+            }
+        }
+    }
+}
+
+
+function drawLine(x1, y1, x2, y2) {
+    context.beginPath();
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
+}
+	
