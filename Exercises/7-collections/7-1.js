42b8eb2dd76da307c67bb43dfcfc3ad4a674b083
zineb mettioui <zineb.mettioui@student.ehb.be>
2022-12-15 10:38:55 +0100

    7-1

---
+++ Exercises/7-collections/7-1.js
@@ -0,0 +1,34 @@
+"use strict";
+
+setup();
+
+function setup()
+{
+    let numbers = [];
+
+    for (let i = 0; i < 100; i++)
+    {
+        numbers[i] = Math.random() * 100;
+    }
+
+    let numbers2 = [];
+
+
+    for (let i = 0; i < 200; i++)
+    {
+        numbers2[i] = Math.random() * 100;
+    }
+
+    console.log(calculateAverage(numbers));
+    console.log(calculateAverage(numbers2));
+}
+
+function calculateAverage(numbers)
+{
+    let total = 0;
+    for (let i = 0; i < numbers.length; i++)
+    {
+        total += numbers[i];
+    }
+    return total / numbers.length;
+}
