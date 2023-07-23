ec38dab73064c56eaf369161a89f4a8d94d62813
zinebmettioui <114058287+zinebmettioui@users.noreply.github.com>
2023-07-23 20:08:57 +0200

    exercise 1-4-layers done

--- TweedeZit/Exercises/1-functions/1-4-layers.js
+++ TweedeZit/Exercises/1-functions/1-4-layers.js
@@ -12,33 +12,9 @@ function drawLayers() {
 	context.lineWidth = 2;
 	context.fillStyle = "lightblue";
 	context.rect(50, 50, 300, 300);
-	context.fill();
-	context.stroke();
-
-	context.beginPath();
-	context.lineWidth = 2;
-	context.fillStyle = "lightblue";
 	context.rect(50, 50, 250, 250);
-	context.fill();
-	context.stroke();
-
-   context.beginPath();
-	context.lineWidth = 2;
-	context.fillStyle = "lightblue";
 	context.rect(100, 50, 200, 200);
-	context.fill();
-	context.stroke();
-
-   context.beginPath();
-	context.lineWidth = 2;
-	context.fillStyle = "lightblue";
 	context.rect(150, 100, 150, 150);
-	context.fill();
-	context.stroke();
-
-   context.beginPath();
-	context.lineWidth = 2;
-	context.fillStyle = "lightblue";
 	context.rect(150, 150, 100, 100);
 	context.fill();
 	context.stroke();
