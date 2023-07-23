946c835f4cbca101a2a0a72a93aecfb1431be674
zinebmettioui <114058287+zinebmettioui@users.noreply.github.com>
2023-07-23 21:28:01 +0200

    exercise 1-6-starrynight doing

--- TweedeZit/Exercises/1-functions/1-6-starrynight.js
+++ TweedeZit/Exercises/1-functions/1-6-starrynight.js
@@ -7,15 +7,51 @@ let context = canvas.getContext("2d");
 
 drawStarryNight();
 
-function drawStarryNight() { 
+function drawStarryNight() {
+	context.lineWidth = 10;
+	context.fillStyle = "orange";
 
-context.lineWidth = 5; 
-context.strokeStyle = "orange"; 
-context.strokeRect(50, 50, 300, 300); 
+	// guides lines
+	context.strokeStyle = "blue";
+	context.beginPath();
+	context.moveTo(50, 50);
+	context.lineTo(350, 350);
+	context.moveTo(100, 50);
+	context.lineTo(350, 300);
+	context.moveTo(150, 50);
+	context.lineTo(350, 250);
+	context.moveTo(50, 100);
+	context.lineTo(300, 350);
+	context.moveTo(50, 150);
+	context.lineTo(250, 350);
 
+	context.moveTo(350, 50);
+	context.lineTo(50, 350);
+	context.moveTo(300, 50);
+	context.lineTo(50, 300);
+	context.moveTo(250, 50);
+	context.lineTo(50, 250);
+	context.moveTo(100, 350);
+	context.lineTo(350, 100);
+	context.moveTo(150, 350);
+	context.lineTo(350, 150);
+	context.stroke();
 
+	// Center square
+	context.beginPath();
+	context.moveTo(200, 150);
+	context.lineTo(250, 200);
+	context.lineTo(200, 250);
+	context.lineTo(250, 150);
+	context.lineTo(150, 200);
+	context.fill();
 
+	// square
+	context.strokeStyle = "orange";
+	context.strokeRect(50, 50, 300, 300);
 
-
-
+	// text
+	context.textAlign = "center";
+	context.font = "normal 30pt Arial";
+	context.fillText(" STARRY NIGHT", 200, 400);
 }
